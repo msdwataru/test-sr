93db09284ed2be8d58921a60842ec3fe1adddab2
msdwtr <ssin526wm@gmail.com>
2023-02-12 09:36:55 +0900

    ans4

--- README.md
+++ README.md
@@ -13,13 +13,20 @@ cp .env.sample .env
 pipenv sync
 ```
 
-## 実行
+## 回答
 ### ans1
+下記を実行
 ```
 pipenv run python ans1.py --num_fetched 100
 ```
 
 ### ans2
+下記を実行
 ```
 pipenv run python ans2.py --num_fetched 10
 ```
+
+### ans3
+
+### ans4
+`ans4.md`に記載
--- ans2.py
+++ ans2.py
@@ -13,6 +13,8 @@ args = parser.parse_args()
 if __name__ == "__main__":
     three_days_before = (datetime.now(timezone.utc) -
                          timedelta(days=3)).isoformat()
+
+    # NOTE 人気を視聴数（viewCount）ととらえたが、評価の高い順（rating）かもしれない
     res = YoutubeDataAPIClient().fetch_url_as_list("Apex Legends", args.num_fetched,
                                                    order="viewCount", region_code="JP", published_after=three_days_before)
     print("\n".join(res))
